99e777124a7003a10172534bc0b0e0b52dd5c049
Yehezkiel A <yehez30x@gmail.com>
2022-04-25 09:40:49 +0700

    Change ' to "

--- components/header/mobileHeader.js
+++ components/header/mobileHeader.js
@@ -5,19 +5,19 @@ const MobileHeader = ({minimizeMenu}) => {
     return (
         <div className="flex flex-col bg-neutral-900 h-screen my-10 space-y-4" >
             <div onClick={() => minimizeMenu(false)}>
-                <Link href='/' customClass={'text-3xl'} >
+                <Link href="/" customClass={"text-3xl"} >
                     Home
                 </Link>
             </div>
 
             <div onClick={() => minimizeMenu(false)}>
-                <Link href='/about' customClass={'text-3xl'}>
+                <Link href="/about" customClass={"text-3xl"}>
                     About
                 </Link>
             </div>
 
             <div onClick={() => minimizeMenu(false)}>
-                <Link href='/projects' customClass={'text-3xl'}>
+                <Link href="/projects" customClass={"text-3xl"}>
                     Projects
                 </Link>
             </div>
