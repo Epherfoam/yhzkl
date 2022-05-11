8ffb8fe8464ff7ab4cdf0eabd0cc896339a42b41
Yehezkiel A <yehez30x@gmail.com>
2022-05-12 00:21:25 +0700

    fix: wait, did an oopsie

--- components/index/hero.js
+++ components/index/hero.js
@@ -2,7 +2,7 @@ import Emphasize from "../emphasize";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col-reverse text-center h-screen justify-center md:flex-row md:justify-between md:text-left w-full pt-4">
+    <div className="flex flex-col-reverse text-center h-screen justify-center md:flex-row md:justify-between md:text-left w-full pt-4 md:pt-0">
       <div className="flex flex-col space-y-4 justify-center content-center w-full md:w-4/5">
         <Emphasize customClass={"text-4xl"}>こんにちわssup!</Emphasize>
         <h1 className="w-full">
